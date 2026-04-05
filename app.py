1bc5649405979cdc93b31a8c0d4e23cb957549b1
ptanay <potnurutanay2005@gmail.com>
2026-04-05 12:15:55 +0530

    Update app.py
    fixed port fir deployment

--- app.py
+++ app.py
@@ -335,4 +335,5 @@ def delete_booking(booking_id):
 # ================= RUN =================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT" ,5000))
+    app.run(host="0.0.0.0", port=port)
